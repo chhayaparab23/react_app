da5772c1f678fe4d9e6753c57b70272f0919433a
chhayap7658 <parab.chhaya13@gmail.com>
2020-08-28 09:02:21 -0400

    Updates

---
+++ precache-manifest.93bc912bd37a733485403d645d8cbc5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a29eeacf691a78001831abbddb1ace6",
+    "url": "/react_app/index.html"
+  },
+  {
+    "revision": "51474e35e881a69cdc84",
+    "url": "/react_app/static/css/main.dc08a8c3.chunk.css"
+  },
+  {
+    "revision": "8edf7eb235a83b691af3",
+    "url": "/react_app/static/js/2.c55b3ee9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_app/static/js/2.c55b3ee9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51474e35e881a69cdc84",
+    "url": "/react_app/static/js/main.5b1f16a4.chunk.js"
+  },
+  {
+    "revision": "c24a5545cbe9b1e63fd3",
+    "url": "/react_app/static/js/runtime-main.8675c917.js"
+  }
+]);
